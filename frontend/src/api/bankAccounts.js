13af7010f15ac068da88334184b1eff8c1852e08
Kevin diaz <kjcontreras@cohorsil.hn>
2026-06-12 15:31:03 -0600

    fix: partners y bankAccounts usan axios con JWT token

--- frontend/src/api/bankAccounts.js
+++ frontend/src/api/bankAccounts.js
@@ -1,8 +1,4 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
-});
+import api from './axios';
 
 export const getBankAccounts = () => api.get('/bank-accounts');
 export const getBankAccountById = (id) => api.get(`/bank-accounts/${id}`);
--- frontend/src/api/partners.js
+++ frontend/src/api/partners.js
@@ -1,8 +1,4 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:3000/api',
-});
+import api from './axios';
 
 export const getPartners = () => api.get('/partners');
 export const getPartnerById = (id) => api.get(`/partners/${id}`);
